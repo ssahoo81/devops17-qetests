6181d34463ed3c4a769cec110bcd9b9dc9a4ff87
aruntestarun <45982564+aruntestarun@users.noreply.github.com>
2019-10-02 14:26:02 -0700

    Create AppTest.java

---
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -0,0 +1,28 @@
+package com.sndevops.eng;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    private String INPUT = "44";
+    private String INPUT1 = "1234567";
+    private String INPUT2 = "115";
+    private boolean isPassed = true;
+    // Add more tests
+    // Connected , unconfigured track true- Case 1
+    @Test
+    public void testLength() {
+        // Test with new Dev build 1
+        Assert.assertEquals(INPUT,"44");
+        Assert.assertEquals(INPUT2,"115");
+    }
+    
+    @Test
+    public void testPassed() {
+        // Add test cases 111
+        // Add few lines
+        Assert.assertEquals(isPassed,true);
+        Assert.assertEquals(isPassed,true);
+    }
+}
