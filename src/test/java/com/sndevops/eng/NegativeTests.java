4ef05f7a5e0bc3858b8a67c5f15c9b22409ae6ad
aruntestarun <45982564+aruntestarun@users.noreply.github.com>
2019-10-03 14:56:07 -0700

    Create NegativeTests.java

---
+++ src/test/java/com/sndevops/eng/NegativeTests.java
@@ -0,0 +1,23 @@
+package com.sndevops.eng;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Ignore;
+
+public class AppTest {
+    private String INPUT = "43";
+
+        // public void testQETest11() throws Exception{
+        // Test with new Dev build 1
+        // Assert.assertEquals(INPUT,"4");
+
+    // }
+
+
+// @Test public void testQETest12(){}
+    @Ignore
+    @Test public void testQETest13(){}
+
+    
+}
