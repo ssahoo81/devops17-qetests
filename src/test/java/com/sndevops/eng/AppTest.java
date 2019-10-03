b6467e1174e73b8a0d01545455f5cabef75a18fc
aruntestarun <45982564+aruntestarun@users.noreply.github.com>
2019-10-03 14:54:39 -0700

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -7,11 +7,7 @@ import org.junit.Ignore;
 
 public class AppTest {
     private String INPUT = "43";
-    private String INPUT1 = "1234567";
-    private String INPUT2 = "115";
-    private boolean isPassed = true;
-    // Add more tests
-    // Connected , unconfigured track true- Case 1
+
     @Test
     public void testQETest1() throws Exception {
         // Test with new Dev build 1
@@ -23,67 +19,67 @@ public class AppTest {
     public void testQETest2() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+
                 Thread.sleep(5000);
     }
         @Test
     public void testQETest3() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+
                 Thread.sleep(5000);
     }
         @Test
     public void testQETest4() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+
     }
         @Test
     public void testQETest5() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+  
     }
         @Test
     public void testQETest6() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+
     }
         @Test
     public void testQETest7() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+
     }
         @Test
     public void testQETest8() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+   
     }
         @Test
     public void testQETest9() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+
     }    @Test
     public void testQETest10() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+ 
     }
         public void testQETest11() throws Exception{
         // Test with new Dev build 1
         Assert.assertEquals(INPUT,"4");
-        Assert.assertEquals(INPUT2,"115");
+
     }
 
 
-
-
 @Test public void testQETest12(){}
+    @Ignore
+    @Test public void testQETest13(){}
 
     
 }
